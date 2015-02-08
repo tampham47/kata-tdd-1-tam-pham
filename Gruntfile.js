2232582ac6168428a22d874db3b5b353b0ba08c0
Tam Pham <tampham47@live.com>
2015-02-08 22:39:58 +0700

    add jshint, grunt-karma, grunt-watch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,35 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'source/**/*.js',
+        'test/**/*.js'
+      ]
+    },
+
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+
   });
 
-  require('load-grunt-tasks')(grunt);
-  grunt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('default', ['jshint', 'watch']);
+  grunt.registerTask('test', ['jshint', 'karma']);
 };
