ea4bac3799be643eb4b33d0c8f5fa2ca5beea723
Tam Pham <tampham47@live.com>
2015-02-08 13:46:11 +0700

    handle an unknown amount of numbers and refactoring

--- source/calculator.js
+++ source/calculator.js
@@ -2,10 +2,13 @@
 
 var calculator = {
   add: function(nums) {
-    var
-      pices = nums.split(','),
-      result = 0;
+    var pices = nums.split(/[\n,]+/);
 
+    return this.calculatorSum(pices);
+  },
+
+  calculatorSum: function(pices){
+    var result = 0;
     for (var i=0; i<pices.length; i++) {
       if (!isNaN(parseInt(pices[i] || 0)))
         result += parseInt(pices[i] || 0);
--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -2,7 +2,7 @@
 
 describe('calculator', function(){
   function checkResult(express, value) {
-    it('should evaluate ' + express + ' to ' + value, function(){
+    it('should evaluate "' + express + '" to ' + value, function(){
       expect(calculator.add(express)).toEqual(value);
     });
   }
@@ -13,9 +13,14 @@ describe('calculator', function(){
     checkResult('1,2', 3);
   });
 
-  describe('handle unknown amount of numbers', function(){
+  describe('unknown amount', function(){
     checkResult('1,2,k', 3);
   });
+
+  describe('new lines', function(){
+    checkResult('1\n2', 3);
+    checkResult('1,2\n5', 8);
+  })
 
 });
 
